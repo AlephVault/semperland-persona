a4b34dcfe9048ec25a270461bfe404120750fedc
Luis Masuelli <luisfmasuelli@gmail.com>
2026-07-23 11:39:11 -0500

    Adding the lot_downloader static variable.

--- packages/addons/Semperland.Persona/visuals/persona.gd
+++ packages/addons/Semperland.Persona/visuals/persona.gd
@@ -3,6 +3,11 @@ extends AlephVault__WindRose__REFMAP.Contrib.Citizens.Visuals.Citizen
 ## a component using "1/" strips that prefix.
 
 const _DefaultResolver = AlephVault__WindRose__REFMAP.Utils.DefaultResolver
+const _LotDownloader = Semperland__Persona.Utils.LotDownloader
+
+## Use this property to set a Persona-wide lot downloader.
+## Useful to resolve and download new lots dynamically.
+static var lot_downloader: _LotDownloader = null
 
 static func _is_default_resolver() -> bool:
 	var resolver_obj = AlephVault__WindRose__REFMAP.Visuals.People.resolver
